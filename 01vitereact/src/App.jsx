12df8f2c34b8d928855104a2a83a3e43149e2390
Suryansh Kumar <suryansh7250@gmail.com>
2024-11-06 12:37:07 +0530

    Setting up React

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Chai from './chai';
+
+function App() {
+  const username = "chai aur code"
+
+  return (
+    <>
+    <Chai />
+    <h1>Chai Aur React {username}</h1>
+    </>
+  )
+}
+
+export default App
